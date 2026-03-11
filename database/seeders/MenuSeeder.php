e1b89c2b79f68531d06ae777f3401665c76e0356
User <user@example.com>
2026-03-11 10:18:14 +0700

    Add direct 'Sinkron Excel' and 'Sinkron Word' menus to sidebar

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -62,6 +62,20 @@ class MenuSeeder extends Seeder
                 'order' => 7,
                 'is_active' => true,
             ],
+            [
+                'name' => 'Sinkron Excel',
+                'url' => 'surat.excel',
+                'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" /></svg>',
+                'order' => 8,
+                'is_active' => true,
+            ],
+            [
+                'name' => 'Sinkron Word',
+                'url' => 'surat.word',
+                'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>',
+                'order' => 9,
+                'is_active' => true,
+            ],
         ];
 
         foreach ($menus as $menu) {
